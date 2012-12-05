524cb13986f51def13ee9eb720304135479ed08f
Jan-Willem Buurlage <jwbuurlage@me.com>
2012-12-05 22:33:49 +0100

    started on tool for converting heightmaps

---
+++ tools/16-bit-to-8-bit-converter.cpp
@@ -0,0 +1,8 @@
+// 16-bit TIFF --> TGA CONVERTER
+// Takes a 16-bit TIFF and converts it into a 8-bit TGA file, with the first
+// 8 bits in the RED component, en the second 8 bits in the GREEN component.
+
+int main()
+{
+
+}
