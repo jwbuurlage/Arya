f0fe5fecd166b823d6cb72cfa15686f347cc466f
Abe <abewits@gmail.com>
2012-12-16 14:09:20 +0100

    Using file manager instead of filstream

--- src/Material.cpp
+++ src/Material.cpp
@@ -1,7 +1,7 @@
 #include "Material.h"
+#include "Files.h"
 #include "common/logger.h"
 #include <vector>
-#include <fstream>
 
 namespace Arya
 {
@@ -17,16 +17,19 @@ namespace Arya
 		std::string name=filename.substr(0,filename.size()-4);
 		std::string type;
 		float a,b,c,d;
-		std::ifstream fin(name+".matty");
-		if(fin.good()) {
-			fin >> type >> a >> b >> c >> d;
-		} else {
+		File* mattyfile = FileSystem::shared().getFile(name+".matty");
+		if(mattyfile == 0) {
 			type="Default";
 			a=1.0;
 			b=1.0;
 			c=0.3;
 			d=0.7;
+		} else {
+			std::stringstream str(mattyfile->getData());
+			str >> type >> a >> b >> c >> d;
 		}
+
+		FileSystem::shared().releaseFile(mattyfile);
 
 		Material* result=new Material(name , TextureManager::shared().getTexture(filename), type, a, b, c, d);
 		addResource(filename, result);
