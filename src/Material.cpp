bf67331be63584c8e44666c823e93f12b1cccfd9
Abe <abewits@gmail.com>
2012-12-16 03:20:25 +0100

    Forgot to add material.cpp

---
+++ src/Material.cpp
@@ -0,0 +1,34 @@
+#include "Material.h"
+#include "common/logger.h"
+#include <vector>
+#include <fstream>
+
+namespace Arya
+{
+	template<> MaterialManager* Singleton<MaterialManager>::singleton = 0;
+
+	bool MaterialManager::initialize(std::vector<std::string> filenames) {
+		for(int i=0; i<filenames.size(); i++)
+			getResource(filenames[i]);
+		return true;
+	}
+
+	Material* MaterialManager::loadResource(std::string filename) {
+		std::string name=filename.substr(0,filename.size()-4);
+		std::string type;
+		float a,b,c,d;
+		std::ifstream fin(name+".matty");
+		if(fin.good()) {
+			fin >> type >> a >> b >> c >> d;
+		}
+
+		Material* result=new Material(name , TextureManager::shared().getTexture(filename), type, a, b, c, d);
+		addResource(filename, result);
+		return result;
+	}
+
+	void MaterialManager::cleanup() {
+		unloadAll();
+	}
+
+}
