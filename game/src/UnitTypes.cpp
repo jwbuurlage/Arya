43a5ee9262e9c94f165a1fa73761f9065d30a969
Jan-Willem Buurlage <jwbuurlage@me.com>
2012-12-19 17:08:22 +0100

    added unittypes

---
+++ game/src/UnitTypes.cpp
@@ -0,0 +1,6 @@
+#include "../include/UnitTypes.h"
+
+UnitInfo infoForUnitType[] = {
+    UnitInfo("ogros", 5.0f, 5.0f, 10.0f, 720.0f, 100.0f, 20.0f, 1.0f),
+    UnitInfo("hep", 10.0f, 5.0f, 10.0f, 720.0f, 100.0f, 20.0f, 1.0f)
+};
