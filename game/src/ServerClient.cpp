31b28c94bb9dae0b1d03cb60569d1947af971f31
Tom Bannink <tombannink@gmail.com>
2012-12-22 03:22:02 +0100

    more debug test

--- game/src/ServerClient.cpp
+++ game/src/ServerClient.cpp
@@ -21,11 +21,11 @@ void ServerClient::createStartUnits()
 
     for(int i = 0; i < 20; ++ i) 
     {
-        Unit* unit = gameSession->createUnit(0);
+        //Unit* unit = gameSession->createUnit(0);
+        Unit* unit = gameSession->UnitFactory::createUnit(i, 0);
+        LOG_INFO("TEST UNIT ID " << unit->getId());
         unit->setPosition(vec3(-(faction->getId()%5) * 100.0f + 20.0f * (i / 10), 0.0f, -50.0f + 20.0f * (i % 10)));
         faction->addUnit(unit);
-
-        LOG_INFO("TEST UNIT ID " << unit->getId());
 
         unit = gameSession->createUnit(1);
         unit->setPosition(vec3(-(faction->getId()%5) * 100.0f - 50.0f + 20.0f * (i / 10), 0.0f, -50.0f + 20.0f * (i % 10)));
