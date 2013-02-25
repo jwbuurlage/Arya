0a9039c8fff8f07773d18328f6496e064394cf2d
Tom Bannink <tombannink@gmail.com>
2013-02-25 21:53:53 +0100

    Unit starting positions in corner

--- game/src/Game.cpp
+++ game/src/Game.cpp
@@ -44,7 +44,7 @@ void Game::run()
         network = new Network;
 
         network->startServer();
-        network->connectToLobbyServer("localhost", 13337);
+        network->connectToLobbyServer("balubu.com", 13337);
 
         if(eventManager) delete eventManager;
         eventManager = new EventManager(network);
@@ -68,10 +68,10 @@ void Game::run()
 		lobbyToGameEvent << 10104;
         lobbyToGameEvent.send();
 		//in a normal scenario the following is done below in the eventhandler at EVENT_SESSION_INFO
-		network->connectToSessionServer("localhost", 13337);
+		network->connectToSessionServer("balubu.com", 13337);
 		Event& joinEvent = eventManager->createEvent(EVENT_JOIN_GAME);
 		joinEvent << 50505; //session hash
-		joinEvent << 10102; //my secret hash
+		joinEvent << 10104; //my secret hash
 		joinEvent.send();
 		//END TEMPORARY
 
