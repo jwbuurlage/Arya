abf8790ef784364466c5abf102de7236a294869c
elumens <lumenserik@gmail.com>
2013-02-25 20:44:16 +0100

    fixed some minor bugs

--- src/Root.cpp
+++ src/Root.cpp
@@ -33,6 +33,7 @@ namespace Arya
         scene = 0;
         oldTime = 0;
         overlay = 0;
+        consoleInit = false;
 
         FileSystem::create();
         TextureManager::create();
@@ -101,6 +102,7 @@ namespace Arya
           LOG_INFO("Could not initialize console");
           return false;
         }
+        consoleInit = true;
         addFrameListener(console);
         addInputListener(console);
 
