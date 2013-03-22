d147f5fdd1c95793cb98da7afa90d8f3f2a0b9cf
Jan-Willem Buurlage <jwbuurlage@me.com>
2013-03-23 00:07:26 +0100

    Change UnitType to a class, and link with lua library

--- game/include/UnitTypes.h
+++ game/include/UnitTypes.h
@@ -12,58 +12,59 @@ void registerNewUnitInfo(UnitInfo* info);
 
 //This class is subclassed by LuaUnitType
 //The class instances are created in the scripts
-struct UnitInfo
+class UnitInfo
 {
-    UnitInfo(int type) : typeId(type) { registerNewUnitInfo(this); }
+	public:
+		UnitInfo(int type) : typeId(type) { registerNewUnitInfo(this); }
 
-    UnitInfo(int type, const char* name,
-            float _radius, float _attackRadius, float _viewRadius,
-            float _speed, float _yawSpeed,
-            float _maxHealth, float _damage, float _attackSpeed, bool _canMoveWhileAttacking,
-            const char* _selectionSound, const char* _attackSound)
-        : typeId(type)
-    {
-        registerNewUnitInfo(this);
-        displayname = name;
-        modelname = name;
-        radius = _radius;
-        attackRadius = _attackRadius;
-        viewRadius = _viewRadius;
-        speed = _speed;
-        yawSpeed = _yawSpeed;
-        maxHealth = _maxHealth;
-        damage = _damage;
-        attackSpeed = _attackSpeed;
-        canMoveWhileAttacking = _canMoveWhileAttacking;
-        selectionSound = _selectionSound;
-        attackSound = _attackSound;
-    }
-    virtual ~UnitInfo(){}
+		UnitInfo(int type, const char* name,
+				float _radius, float _attackRadius, float _viewRadius,
+				float _speed, float _yawSpeed,
+				float _maxHealth, float _damage, float _attackSpeed, bool _canMoveWhileAttacking,
+				const char* _selectionSound, const char* _attackSound)
+			: typeId(type)
+		{
+			registerNewUnitInfo(this);
+			displayname = name;
+			modelname = name;
+			radius = _radius;
+			attackRadius = _attackRadius;
+			viewRadius = _viewRadius;
+			speed = _speed;
+			yawSpeed = _yawSpeed;
+			maxHealth = _maxHealth;
+			damage = _damage;
+			attackSpeed = _attackSpeed;
+			canMoveWhileAttacking = _canMoveWhileAttacking;
+			selectionSound = _selectionSound;
+			attackSound = _attackSound;
+		}
+		virtual ~UnitInfo(){}
 
-    //Call these from the game
-    //They will call the appropriate script
-    virtual void onDeath(Unit* unit){};
-    virtual void onSpawn(Unit* unit){};
-    virtual void onDamage(Unit* victim, Unit* attacker, float damage){};
+		//Call these from the game
+		//They will call the appropriate script
+		virtual void onDeath(Unit* unit){};
+		virtual void onSpawn(Unit* unit){};
+		virtual void onDamage(Unit* victim, Unit* attacker, float damage){};
 
-    const int typeId;
+		const int typeId;
 
-    string displayname;
-    string modelname;
+		string displayname;
+		string modelname;
 
-    float radius;
-    float attackRadius;
-    float viewRadius;
-    float speed;
-    float yawSpeed; //in degrees
+		float radius;
+		float attackRadius;
+		float viewRadius;
+		float speed;
+		float yawSpeed; //in degrees
 
-    float maxHealth;
-    float damage;
-    float attackSpeed; //the time one attack takes
-    bool canMoveWhileAttacking;
+		float maxHealth;
+		float damage;
+		float attackSpeed; //the time one attack takes
+		bool canMoveWhileAttacking;
 
-    string selectionSound;
-    string attackSound;
+		string selectionSound;
+		string attackSound;
 };
 
 
